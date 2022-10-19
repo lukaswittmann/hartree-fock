b3bc490501613615e0f46616fee3aaeff6957c02
lukaswittmann <lukas.wittmann@live.de>
2022-10-19 18:56:53 +0200

    bugs in integrals partly fixed

--- app/main.f90
+++ app/main.f90
@@ -6,14 +6,19 @@ use integrals
 use basis
 
 real(dp), dimension(2,3) :: molecule_coords
+real(dp), dimension(2) :: z
 type(primitive_gaussian), allocatable :: molecule(:,:)
 
 molecule_coords(1,:) = (/0.,0.,0./)  ! Position of H1
 molecule_coords(2,:) = (/0.,0.,1.6/) ! Position of H2 in Bohr
 
 molecule = def_molecule(molecule_coords)
+z = (/1.0, 1.0/)
 
 call overlap(molecule)
+print *
 call kinetic_energy(molecule)
+print *
+call en_interaction(molecule, z)
 
 end program main
--- src/integrals.f90
+++ src/integrals.f90
@@ -1,17 +1,22 @@
 module integrals
-
 use types
 use constants
 use basis
 
+use stdlib_specialfunctions_gamma!, only: lig => lower_incomplete_gamma
+
+
 contains
 
 subroutine overlap(molecule)
+    implicit none
     type(primitive_gaussian), intent(in) :: molecule(:,:)
-    integer :: nbasis, nprimitives_i, nprimitives_j, i, j, k, l
+    integer :: nbasis, i, j, k, l
     real(dp) :: norm, p, q, coeff, Kab
     real(dp), dimension(3) :: Q_xyz
     real(dp), dimension(INT(size(molecule,1)),INT(size(molecule,1))) :: S
+
+    S = 0
 
     nbasis = size(molecule,1)
 
@@ -20,12 +25,9 @@ subroutine overlap(molecule)
     do i = 1, nbasis
         do j = 1, nbasis
                 
-        nprimitives_i = size(molecule(i,:))
-        nprimitives_j = size(molecule(j,:))
-
         ! Iterate over l and m primitives in basis
-        do k = 1, nprimitives_i
-            do l = 1, nprimitives_j
+        do k = 1, size(molecule(i,:)) ! Number of primitives in i..
+            do l = 1, size(molecule(j,:))
 
             norm = molecule(i, k)%norm() * molecule(j, l)%norm()
             ! Eq. 63
@@ -52,11 +54,14 @@ end subroutine overlap
 
 subroutine kinetic_energy(molecule)
     type(primitive_gaussian), intent(in) :: molecule(:,:)
-    integer :: nbasis, nprimitives_i, nprimitives_j, i, j, k, l, m
+    integer :: nbasis, i, j, k, l, m
     real(dp), dimension(INT(size(molecule,1)),INT(size(molecule,1))) :: T
 
     real(dp) :: norm, p, q, coeff, Kab, S
     real(dp), dimension(3) :: Q_xyz, gP, Pp, PG
+
+    S = 0
+    T = 0
 
     nbasis = size(molecule,1)
 
@@ -64,13 +69,10 @@ subroutine kinetic_energy(molecule)
     ! G A U S S I A N BASIS SETS A N D MOLECULAR I N T E G R A L S
     do i = 1, nbasis
         do j = 1, nbasis
-                
-        nprimitives_i = size(molecule(i,:))
-        nprimitives_j = size(molecule(j,:))
 
         ! Iterate over l and m primitives in basis
-        do k = 1, nprimitives_i
-            do l = 1, nprimitives_j
+        do k = 1, size(molecule(i,:))
+            do l = 1, size(molecule(j,:))
 
             coeff = molecule(i, k)%coeff * molecule(j, l)%coeff
             norm = molecule(i, k)%norm() * molecule(j, l)%norm()
@@ -102,5 +104,87 @@ subroutine kinetic_energy(molecule)
     print *, T
 
 end subroutine kinetic_energy
+
+
+
+subroutine en_interaction(molecule, z)
+    implicit none
+    type(primitive_gaussian), intent(in) :: molecule(:,:)
+    real(dp), dimension(2), intent(in) :: z
+    integer :: atom, natoms, nbasis, i, j, k, l
+
+    real(dp), dimension(INT(size(molecule,1)),INT(size(molecule,1))) :: V_ne
+    real(dp) :: norm, p, q, coeff, Kab, S, boys, x, n = 0.0
+    real(dp), dimension(3) :: Q_xyz, gP, Pp, PG
+    
+    V_ne = 0
+
+    natoms = size(molecule, 1) / 2
+    nbasis = size(molecule, 1)
+
+    ! Integral found in https://www.mathematica-journal.com/2014/12/08/evaluation-of-gaussian-molecular-integrals-4/
+    do atom = 1, natoms
+        do i = 1, nbasis
+            do j = 1, nbasis
+
+            do k = 1, size(molecule(i,:))
+                do l = 1, size(molecule(j,:))
+
+                norm = molecule(i, k)%norm() * molecule(j, l)%norm()
+                ! Eq. 63
+                Q_xyz = (molecule(i, k)%coords - molecule(j, l)%coords)
+                ! Eq. 64, 65
+                p = (molecule(i, k)%alpha + molecule(j, l)%alpha)
+                q = (molecule(i, k)%alpha * molecule(j, l)%alpha) / p 
+                ! Eq. 66
+                Kab = exp(-q * dot_product(Q_xyz,Q_xyz))
+                    
+                coeff = molecule(i, k)%coeff * molecule(j, l)%coeff
+
+                gP = (molecule(i, k)%alpha * molecule(i, k)%coords + molecule(j, l)%alpha * molecule(j, l)%coords)
+                Pp = gP / p
+                PG = Pp - molecule(j, l)%coords
+
+                ! Calculate Boys Function
+                x = (p * dot_product(PG,PG))
+                if (x == 0.) then
+                    boys = 1. / (2. * n + 1)
+                else 
+                    boys = regularized_gamma_p(n + 0.5, x) * gamma(n + 0.5) * (1. / (2.  * x ** (n + 0.5)))
+                end if
+
+                V_ne(i,j) =  V_ne(i,j) - z(atom) * norm * coeff * Kab * (2.0 * pi / p) * boys
+                if (coeff /= 0.0) then
+                    print *, p , PG, dot_product(PG,PG)
+                end if
+
+                end do
+            end do
+            end do
+        end do
+    end do
+
+    print *, V_ne
+
+end subroutine en_interaction
+
+
+subroutine ee_interaction(molecule)
+    type(primitive_gaussian), intent(in) :: molecule(:,:)
+    integer :: nbasis, nprimitives_i, nprimitives_j, nprimitives_k, nprimitives_l, i, j, k, l
+
+    real(dp), dimension(INT(size(molecule,1)),INT(size(molecule,1))) :: V_ne   
+    real(dp) :: norm, p, q, coeff, Kab, S, boys, x, n = 0.0
+    real(dp), dimension(3) :: Q_xyz, gP, Pp, PG
+
+    nbasis = size(molecule, 1)
+
+
+
+end subroutine ee_interaction
+
+
+
+
 
 end module integrals
